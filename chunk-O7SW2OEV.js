cd88cbee0f5973676112884d0dee0f7ed7e377ef
omar turky <omarturky110@gmail.com>
2026-07-18 20:12:55 +0300

    fix deployment 18-7

---
+++ chunk-O7SW2OEV.js
@@ -0,0 +1,1 @@
+import{$ as t}from"./chunk-UPE73SHJ.js";var o=class r{rootUrl="";static \u0275fac=function(i){return new(i||r)};static \u0275prov=t({token:r,factory:r.\u0275fac,providedIn:"root"})};export{o as a};
