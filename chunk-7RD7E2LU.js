ed4455f49eaa398f1ca041f4111e272a1d3dc1a6
omar turky <omarturky110@gmail.com>
2026-07-20 16:40:28 +0300

    import spec

---
+++ chunk-7RD7E2LU.js
@@ -0,0 +1,1 @@
+import{X as t}from"./chunk-2FTPRYOL.js";var o=class r{rootUrl="";static \u0275fac=function(i){return new(i||r)};static \u0275prov=t({token:r,factory:r.\u0275fac,providedIn:"root"})};export{o as a};
